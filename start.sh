7c2503e9095c5c613607c795c73b3c55121a875d
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-06-13 21:13:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/Cinderella2022Sub.git /Cinderella2022Sub
+  git clone https://github.com/Sreejithmadmax/Cinderella.git /Cinderella
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Cinderella2022Sub
+  git clone $UPSTREAM_REPO /Cinderella
 fi
-cd /Cinderella2022Sub
+cd /Cinderella
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
