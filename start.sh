38b6c234adb6b6f51ebf8d88ac11c80bd2d00b4a
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-06-14 10:45:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/Cinderella.git /Cinderella
+  git clone https://github.com/Sreejithmadmax/UrvashiTheaters.git /UrvashiTheaters
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Cinderella
+  git clone $UPSTREAM_REPO /UrvashiTheaters
 fi
-cd /Cinderella
+cd /UrvashiTheaters
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
