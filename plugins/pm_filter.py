6942fe1acd9e4a39094eb5abf0ab048801178b82
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-06-14 09:09:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -834,7 +834,7 @@ async def advantage_spell_chok(msg):
     SPELL_CHECK[msg.message_id] = movielist
     btn = [[
         InlineKeyboardButton(
-            text="😄"movie.strip(),
+            text=movie.strip(),
             callback_data=f"spolling#{user}#{k}",
         )
     ] for k, movie in enumerate(movielist)]
