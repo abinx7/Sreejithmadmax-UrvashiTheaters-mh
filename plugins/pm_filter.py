e4f8b0fb902de56a4d2e7b1066696b8d68dc89db
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-02 20:05:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -92,12 +92,12 @@ async def next_page(bot, query):
         ]
     )
 
-    if 0 < offset <= 12:
+    if 0 < offset <= 8:
         off_set = 0
     elif offset == 0:
         off_set = None
     else:
-        off_set = offset - 12
+        off_set = offset - 8
     if n_offset == 0:
         btn.append(
             [InlineKeyboardButton("ʙᴀᴄᴋ", callback_data=f"next_{req}_{key}_{off_set}"),
