9ab92786e0de6a84924254a641b4aa662429efbb
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-14 07:46:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -486,7 +486,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.OWNER_TXT,
+            text=script.ABOUT_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
