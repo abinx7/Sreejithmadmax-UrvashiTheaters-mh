03335d0c369d8c610cdccad62582a0a76672ced9
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-14 07:40:21 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -61,13 +61,13 @@ Pay attention to the following…
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
 
-    ABOUT_TXT = """<b>✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b>"""
+    ABOUT_TXT = """<b>🤖 ᴍʏ ɴᴀᴍᴇ: {}
+👨‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
+📕 ʟɪʙʀᴀʀʏ : <a href=https://t.me/UrvashiTheaters>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+📝 ʟᴀɴɢᴜᴀɢᴇ : <a href=https://t.me/UrvashiTheaters>𝙿𝚈𝚃𝙷𝙾𝙽 𝟹</a>
+💾 ᴅᴀᴛᴀ ʙᴀꜱᴇ : <a href=https://t.me/UrvashiTheaters>𝙼𝙾𝙽𝙶𝙾 𝙳𝙱</a>
+📡 ʙᴏᴛ ꜱᴇʀᴠᴇʀ : <a href=https://t.me/UrvashiTheaters>𝙷𝙴𝚁𝙾𝙺𝚄</a>
+🛠️ ʙᴜɪʟᴅ ꜱᴛᴀᴛᴜꜱ : <a href=https://t.me/UrvashiTheaters>v1.0.1 [ 𝙱𝙴𝚃𝙰 ]</b></a>"""
 
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
