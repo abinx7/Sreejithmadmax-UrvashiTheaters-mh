6cc88f604f2b1b6afc3f3e55ec343a6e143a31c6
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-07-14 11:07:53 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -144,7 +144,7 @@ async def re_enable_chat(bot, message):
 
 @Client.on_message(filters.command('stats') & filters.incoming)
 async def get_ststs(bot, message):
-    rju = await message.reply('Fetching stats..')
+    rju = await message.reply('Fetching stats..', show_alert=True))
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
