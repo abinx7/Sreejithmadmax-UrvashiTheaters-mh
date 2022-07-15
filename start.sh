d20aa4d71ca444fe3483783c6b5d46166f28e0af
abinx7 <108940991+abinx7@users.noreply.github.com>
2022-07-15 11:53:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/UrvashiTheaters.git /UrvashiTheaters
+  git clone https://github.com/abinx7/Sreejithmadmax-UrvashiTheaters-mh.git /MoviesHubGroup2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /UrvashiTheaters
+  git clone $UPSTREAM_REPO /MoviesHubGroup2
 fi
-cd /UrvashiTheaters
+cd /MoviesHubGroup2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
